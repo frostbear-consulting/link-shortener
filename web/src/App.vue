794cc70e6417606458c391a8dcc33a91165755a6
Daniel Koch <k@kdb.gmbh>
2022-04-20 14:18:23 +0200

    Playing with fire

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,57 @@
 <script>
 import 'spectre.css';
+import { useTitle } from '@/lib';
+import Dropdown from '@/components/Dropdown';
 
 export default {
     name: 'App',
+
+    components: {
+        Dropdown,
+    },
+
+    data() {
+        return {
+            name: 'Rainer Zufall',
+            counter: 0,
+        };
+    },
+
+    methods: {
+        changeName() {
+            this.name = 'Dr. Med. Wurst';
+        },
+
+        hovered() {
+            this.counter = (this.counter + 1) % 255;
+        },
+    },
+
+    computed: {
+        style() {
+            return `background: rgb(${this.counter}, ${255 - this.counter}, 0)`;
+        },
+    },
+
+    mounted() {
+        useTitle('Starteseite');
+    },
 };
 </script>
 
 <template>
-    <h1>hello, world</h1>
+    <div>
+        <h2>Hallo: {{ name }}</h2>
+        <button type='button'
+                @click='changeName'
+                @mousemove='hovered'
+                :style='style'
+                class='btn btn-primary'>
+            Click me
+            ({{ counter }})
+        </button>
+
+        <Dropdown></Dropdown>
+        <Dropdown />
+    </div>
 </template>

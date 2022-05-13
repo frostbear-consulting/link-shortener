7460652f6db65c7599161af17e9802e23d9aed57
Daniel Koch <k@kdb.gmbh>
2022-05-13 10:50:03 +0200

    Implement logout

---
+++ web/src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <header class='navbar'>
+        <section class='navbar-section'></section>
+        <section class='navbar-section'>
+            <button type='button' @click='onLogoutClick' class='btn btn-link'>Logout</button>
+        </section>
+    </header>
+
+</template>
+
+<script>
+import { DELETE } from '@/http-client';
+import { session } from '@/stores/session.store';
+
+export default {
+    name: 'Navigation',
+
+    methods: {
+        async onLogoutClick() {
+            await DELETE('api/session');
+            session.commit('signOut');
+
+            this.$router.push({ name: 'login' });
+        },
+    },
+};
+</script>
+
+<style scoped>
+header {
+    background: #fff;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.5);
+    border-radius: 4px;
+    margin: 1rem 0;
+    padding: 0.25rem 0;
+}
+</style>
--- web/src/views/DashboardView.vue
+++ web/src/views/DashboardView.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>hello, world</h1>
+    <Navigation />
 </template>
+
+<script>
+import Navigation from '@/components/Navigation';
+
+export default {
+    name: 'DashboardView',
+
+    components: {
+        Navigation,
+    },
+};
+</script>
